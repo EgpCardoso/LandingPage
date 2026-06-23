7f37ca83cd712722122baab16c5c7103209b4059
EgpCardoso <egpcardoso@minha.fag.edu.br>
2026-06-23 14:51:42 -0300

    Atualização reCAPTCHA

--- netlify/functions/verify-captcha.js
+++ netlify/functions/verify-captcha.js
@@ -2,18 +2,8 @@ import fetch from "node-fetch";
 import nodemailer from "nodemailer";
 
 export async function handler(event) {
-
-  const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
   const { token, email, message } = JSON.parse(event.body);
 
-  // Valida reCAPTCHA
   const secret = process.env.RECAPTCHA_SECRET_KEY;
   const response = await fetch("https://www.google.com/recaptcha/api/siteverify", {
     method: "POST",
@@ -26,7 +16,6 @@ export async function handler(event) {
     return { statusCode: 400, body: JSON.stringify({ success: false }) };
   }
 
-  // Dispara e-mail
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
